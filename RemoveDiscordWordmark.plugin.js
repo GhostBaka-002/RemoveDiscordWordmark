770f0ec687a50081808cd273215c6d13d72598f4
GhostBaka-002 <68557209+GhostBaka-002@users.noreply.github.com>
2022-08-31 12:52:02 -0700

    Update RemoveDiscordWordmark.plugin.js

--- RemoveDiscordWordmark.plugin.js
+++ RemoveDiscordWordmark.plugin.js
@@ -1,9 +1,21 @@
-/*
-------------------------------------------------------
-Gets rid of the stupid wordmark in the top left corner
-By GhostBaka002
-------------------------------------------------------
-*/
-[class="wordmarkWindows-2dq6rw wordmark-2u86JB"] {
-    display: none;
-}
+module.exports = (_ => {
+	const config = {
+		"info": {
+			"name": "RemoveDiscordWordmark",
+			"author": "GhostBaka002",
+			"version": "1.0.0",
+			"description": "I wrote this into my customCss area for BetterDiscord and wanted to see if I could make it work as a plugin"
+		}
+	};
+
+	/*
+    ------------------------------------------------------
+    Gets rid of the stupid wordmark in the top left corner
+    By GhostBaka002
+    ------------------------------------------------------
+    */
+    [class="wordmarkWindows-2dq6rw wordmark-2u86JB"] {
+        display: none;
+    }
+	})(window.BDFDB_Global.PluginUtils.buildPlugin(config));
+})();
